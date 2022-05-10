bb1152a1a3e23eeaffa9dec2994dd4c25f37c02b
Hoang Minh Hieu <hieucoi@MacBook-Pro-cua-Hoang-2.local>
2022-05-10 14:00:42 +0700

    Fix size multibarbutton

--- dist/components/MultiBarButton/Styles.js
+++ dist/components/MultiBarButton/Styles.js
@@ -3,13 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.styles = void 0;
 const react_native_1 = require("react-native");
 exports.styles = react_native_1.StyleSheet.create({
-    contentContainer: {
-        top: -30,
-        width: 60,
-        height: 60,
-        borderRadius: 40,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#333333'
-    }
+  contentContainer: {
+    top: -30,
+    width: 64,
+    height: 64,
+    borderRadius: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#333333",
+  },
 });
